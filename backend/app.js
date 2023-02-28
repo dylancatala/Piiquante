b5305945a1a6e356cdaaf07de2784e7c16f6533c
dylancatala <dylan.catala@proton.me>
2023-02-28 13:57:43 +0100

    Modifying Helmet Security with images

--- backend/app.js
+++ backend/app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 
 
 app.use(express.json());
-//app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 
 app.use("/api/auth", userRoutes);
